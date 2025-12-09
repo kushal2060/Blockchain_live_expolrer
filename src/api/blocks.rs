3afed3d4edce7e27f3f5cb6eb1479922bf69dc58
Kushal <csit22081006_kushal@achsnepal.edu.np>
2025-12-09 13:33:09 +0545

    Endpoints

--- src/api/blocks.rs
+++ src/api/blocks.rs
@@ -1,1 +1,32 @@
 //block endpoints
+use actix_web::{get,web,HttpResponse,Responder};
+use std::sync::Arc;
+use crate::{models::block, oura_stream::BlockChainState};
+
+#[get("/api/blocks")]
+pub async fn get_blocks(
+    state: web::Data<Arc<BlockChainState>>,
+    query: web::Query<std::collections::HashMap<String,String>>,   
+) -> impl Responder {
+    let limit = query .get("limit").and_then(|s| s.parse::<usize>().ok()).unwrap_or(20).min(100); //maximumm 100 blocks
+
+    let blocks =state.get_blocks(limit).await;
+    HttpResponse::Ok().json(serde_json::json!({
+        "blocks":blocks,
+        "count": blocks.len(),
+    }))
+}
+
+#[get("api/blocks/latest")]
+pub async fn get_latest_block(state: web::Data<Arc<BlockChainState>>)-> impl Responder{
+    let blocks = state.get_blocks(1).await;
+
+    if let Some(block) =blocks.first(){
+        HttpResponse::Ok().json(block)
+    }
+    else {
+        HttpResponse::NotFound().json(serde_json::json!({
+            "error": "no blocks avilanble"
+        }))
+    }
+}
--- src/api/transactions.rs
+++ src/api/transactions.rs
@@ -1,1 +1,19 @@
 //transactions endpoint
+
+use actix_web::{get,web,Responder,HttpResponse};
+use std::sync::Arc;
+use crate::{models::transaction, oura_stream::BlockChainState};
+
+#[get("/api/transaction")]
+pub async fn get_transactions(
+    state: web::Data<Arc<BlockChainState>>,
+    query: web::Query<std::collections::HashMap<String,String>>,
+) -> impl  Responder {
+    let limit = query .get("limit").and_then(|s| s.parse::<usize>().ok()).unwrap_or(50).min(200);
+
+    let transactions = state.get_transactions(limit).await;
+    HttpResponse::Ok().json(serde_json::json!({
+        "transactions":transactions,
+        "count":transactions.len(),
+    }))
+}
